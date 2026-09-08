4dc0ccae8586dd954d4ef9ffaf57ae89929c99cb
latiasirl <latiausirl@gmail.com>
2026-09-08 21:10:17 +1000

    Other: Added art and functionality for PZ3, PZ4 and PZ6; added those sets to Standard/Expanded

---
+++ spirit/game/scripts/products/pz3/booster_pz3.py
@@ -0,0 +1,8 @@
+from spirit.game.data_utils import BoosterPackDef
+
+product = BoosterPackDef(
+    guid="e516c1a4-8994-4acd-88ef-73a58b84abf3",
+    key="PZ3",
+    name="Prize Pack Series Three",
+    image_url="pz3_booster"
+)
---
+++ spirit/game/scripts/products/pz4/booster_pz4.py
@@ -0,0 +1,8 @@
+from spirit.game.data_utils import BoosterPackDef
+
+product = BoosterPackDef(
+    guid="9b342c94-7bc7-4bf1-a9b6-56039f8f0893",
+    key="PZ4",
+    name="Prize Pack Series Four",
+    image_url="pz4_booster"
+)
