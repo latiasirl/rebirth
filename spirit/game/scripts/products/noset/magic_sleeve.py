de7516505d0349c224569664786d560903f76fbe
latiasirl <latiausirl@gmail.com>
2026-09-09 12:25:10 +1000

    Other: Added Magic: The Gathering and Yu-Gi-Oh! card sleeves

---
+++ spirit/game/scripts/products/noset/magic_sleeve.py
@@ -0,0 +1,8 @@
+from spirit.game.data_utils import SleeveDef
+
+product = SleeveDef(
+    guid="fdbd738a-d727-4ad9-99c1-c008e12aee86",
+    key="NoSet",
+    name="Magic: The Gathering",
+    image_url="magic"
+)
---
+++ spirit/game/scripts/products/noset/yugioh_sleeve.py
@@ -0,0 +1,8 @@
+from spirit.game.data_utils import SleeveDef
+
+product = SleeveDef(
+    guid="32a68ffc-cbdb-4d77-bd19-f50cb7277762",
+    key="NoSet",
+    name="Yu-Gi-Oh!",
+    image_url="yugioh"
+)
