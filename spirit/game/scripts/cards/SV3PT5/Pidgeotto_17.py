c64500b217bd800b74b16fb979ca0a3c829244b8
latiasirl <latiausirl@gmail.com>
2026-09-12 17:31:50 +1000

    Cards: Implemented Pidgeotto (MEW)

---
+++ spirit/game/scripts/cards/SV3PT5/Pidgeotto_17.py
@@ -0,0 +1,29 @@
+from spirit.game.data_utils import PokemonCardDef, Attack
+from spirit.game.attributes import PokemonStage, PokemonTypes, Rarities
+
+card = PokemonCardDef(
+    guid="8a21bddb-a1a5-497f-8997-6515d89e8e0a",
+    key="SV3PT5",
+    name="com.direwolfdigital.cake.data.archetypes.pokemon.Pidgeotto.Name",
+    display_name="Pidgeotto",
+    searchable_by=["Pidgeotto","Stage 1"],
+    subtypes=["Stage 1"],
+    collector_number=17,
+    set_code="SV3PT5",
+    regulation_mark="G",
+    rarity=Rarities.Common,
+    hp=80,
+    elements=[PokemonTypes.COLORLESS],
+    stage=PokemonStage.BASIC,
+    retreat_cost=0,
+    weakness_type=PokemonTypes.LIGHTNING,
+    weakness_type=PokemonTypes.FIGHTING,
+    evolves_from="com.direwolfdigital.cake.data.archetypes.pokemon.Pidgey.Name",
+    abilities=[
+        Attack(
+            title="Flap",
+            cost={PokemonTypes.COLORLESS: 1},
+            damage=20,
+        ),
+    ],
+)
